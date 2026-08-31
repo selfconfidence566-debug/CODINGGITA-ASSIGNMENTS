8d2ba4d31c5a7ade7f8c5e7e3cea33a34e1b387c
selfconfidence566-debug <selfconfidence566@gmail.com>
2026-08-31 18:52:08 +0530

    Comment out multiple Python assignment questions

--- subjects/python_assignment/topic_3.py
+++ subjects/python_assignment/topic_3.py
@@ -1,1 +1,95 @@
+# Q-21
+# name = "Riya"
+# age = 20
+# marks = 85.5
 
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+
+# print("Name Type:", type(name))
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+# Q-22
+# name = "Riya"
+# age = 20
+# marks = 85.5
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+
+# print("Name Type:", type(name))
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+# Q-23
+# age = "18"
+# marks = 85.5
+
+# print("Age:", age)
+# print("Marks:", marks)
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+# Q-24
+# age = 18
+# marks = "85.5"
+
+# print("Age:", age)
+# print("Marks:", marks)
+
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+# Q-25
+# name = "Ravi"
+# age = 18
+# marks = 90.5
+# passed = True
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+# print("Passed:", passed)
+
+# print(type(name))
+# print(type(age))
+# print(type(marks))
+# print(type(passed))
+
+# Q-26
+# age = "18"
+# marks = 85.5
+
+# print("Age:", age)
+# print("Marks:", marks)
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+
+# Q-27
+# student_name = "Karan"
+# student_age = 18
+# student_marks = 87.5
+
+# print("Student:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+
+# print("Student Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+
+# Q-28
+# student_name = "Karan"
+# student_age = 18
+# student_marks = 87.5
+
+# print("Student:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+
+# print("Student Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
